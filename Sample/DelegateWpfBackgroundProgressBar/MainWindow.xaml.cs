0891339546a3c248ea72775e2f191cf3932272dd
AirVents <antomykl@ukr.net>
2014-11-24 01:05:57 +0200

    j7hh

--- Sample/DelegateWpfBackgroundProgressBar/MainWindow.xaml.cs
+++ Sample/DelegateWpfBackgroundProgressBar/MainWindow.xaml.cs
@@ -1,30 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace DelegateWpfBackgroundProgressBar
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+   
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        DelegateSample sampleDelegate = new DelegateSample();
+        DelegateSample _sampleDelegate = new DelegateSample();
 
         void notify(int step, string msg)
         {
@@ -50,22 +36,22 @@ namespace DelegateWpfBackgroundProgressBar
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
-            DelegateWpfBackgroundProgressBar.BeltGearTempDataSet beltGearTempDataSet = ((DelegateWpfBackgroundProgressBar.BeltGearTempDataSet)(this.FindResource("beltGearTempDataSet")));
-            // Загрузить данные в таблицу Профіль. Можно изменить этот код как требуется.
-            DelegateWpfBackgroundProgressBar.BeltGearTempDataSetTableAdapters.ПрофільTableAdapter beltGearTempDataSetПрофільTableAdapter = new DelegateWpfBackgroundProgressBar.BeltGearTempDataSetTableAdapters.ПрофільTableAdapter();
-            beltGearTempDataSetПрофільTableAdapter.Fill(beltGearTempDataSet.Профіль);
-            System.Windows.Data.CollectionViewSource профільViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("профільViewSource")));
-            профільViewSource.View.MoveCurrentToFirst();
-            // Загрузить данные в таблицу NumbersBelts. Можно изменить этот код как требуется.
-            DelegateWpfBackgroundProgressBar.BeltGearTempDataSetTableAdapters.NumbersBeltsTableAdapter beltGearTempDataSetNumbersBeltsTableAdapter = new DelegateWpfBackgroundProgressBar.BeltGearTempDataSetTableAdapters.NumbersBeltsTableAdapter();
-            beltGearTempDataSetNumbersBeltsTableAdapter.Fill(beltGearTempDataSet.NumbersBelts);
-            System.Windows.Data.CollectionViewSource профільNumbersBeltsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("профільNumbersBeltsViewSource")));
-            профільNumbersBeltsViewSource.View.MoveCurrentToFirst();
-            // Загрузить данные в таблицу Шківи. Можно изменить этот код как требуется.
-            DelegateWpfBackgroundProgressBar.BeltGearTempDataSetTableAdapters.ШківиTableAdapter beltGearTempDataSetШківиTableAdapter = new DelegateWpfBackgroundProgressBar.BeltGearTempDataSetTableAdapters.ШківиTableAdapter();
-            beltGearTempDataSetШківиTableAdapter.Fill(beltGearTempDataSet.Шківи);
-            System.Windows.Data.CollectionViewSource профільNumbersBeltsШківиViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("профільNumbersBeltsШківиViewSource")));
-            профільNumbersBeltsШківиViewSource.View.MoveCurrentToFirst();
+            //DelegateWpfBackgroundProgressBar.BeltGearTempDataSet beltGearTempDataSet = ((DelegateWpfBackgroundProgressBar.BeltGearTempDataSet)(this.FindResource("beltGearTempDataSet")));
+            //// Загрузить данные в таблицу Профіль. Можно изменить этот код как требуется.
+            //DelegateWpfBackgroundProgressBar.BeltGearTempDataSetTableAdapters.ПрофільTableAdapter beltGearTempDataSetПрофільTableAdapter = new DelegateWpfBackgroundProgressBar.BeltGearTempDataSetTableAdapters.ПрофільTableAdapter();
+            //beltGearTempDataSetПрофільTableAdapter.Fill(beltGearTempDataSet.Профіль);
+            //System.Windows.Data.CollectionViewSource профільViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("профільViewSource")));
+            //профільViewSource.View.MoveCurrentToFirst();
+            //// Загрузить данные в таблицу NumbersBelts. Можно изменить этот код как требуется.
+            //DelegateWpfBackgroundProgressBar.BeltGearTempDataSetTableAdapters.NumbersBeltsTableAdapter beltGearTempDataSetNumbersBeltsTableAdapter = new DelegateWpfBackgroundProgressBar.BeltGearTempDataSetTableAdapters.NumbersBeltsTableAdapter();
+            //beltGearTempDataSetNumbersBeltsTableAdapter.Fill(beltGearTempDataSet.NumbersBelts);
+            //System.Windows.Data.CollectionViewSource профільNumbersBeltsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("профільNumbersBeltsViewSource")));
+            //профільNumbersBeltsViewSource.View.MoveCurrentToFirst();
+            //// Загрузить данные в таблицу Шківи. Можно изменить этот код как требуется.
+            //DelegateWpfBackgroundProgressBar.BeltGearTempDataSetTableAdapters.ШківиTableAdapter beltGearTempDataSetШківиTableAdapter = new DelegateWpfBackgroundProgressBar.BeltGearTempDataSetTableAdapters.ШківиTableAdapter();
+            //beltGearTempDataSetШківиTableAdapter.Fill(beltGearTempDataSet.Шківи);
+            //System.Windows.Data.CollectionViewSource профільNumbersBeltsШківиViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("профільNumbersBeltsШківиViewSource")));
+            //профільNumbersBeltsШківиViewSource.View.MoveCurrentToFirst();
 
 
 
